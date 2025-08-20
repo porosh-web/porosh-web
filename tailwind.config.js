c62a748e963cd9273e9429ef73a88e57158b4435
Abid Rahman <porosh.redmi4@gmail.com>
2025-08-21 02:25:46 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4F46E5",
+        secondary: "#9333EA",
+        accent: "#F472B6"
+      },
+    },
+  },
+  plugins: [],
+}
